e2b2be602ae62169e374c7a6c116d052a3eadda3
Welch <CW056762@cerner.net>
2018-06-28 15:02:40 -0500

    fixed everything

---
+++ schedule.bat
@@ -0,0 +1,1 @@
+C:\Users\CW056762\AppData\Local\Continuum\anaconda2\envs\py36\python C:\Users\CW056762\workspace\music_theory_twitter\twitterScraper.py
--- twitterScraper.py
+++ twitterScraper.py
@@ -36,14 +36,15 @@ def grabTweets(api, maxId=None):
 
 def saveData(data):
     if len(data) is not 0:
+        script_path = os.path.dirname(os.path.abspath(__file__))
         df = pd.DataFrame(data)
         print (df)
         cols = df.columns.tolist()
         df = df[[
             "user", "normal", "retweeted", "quoted", "favorited", "id", "timestamp"
         ]]
-        #     with open('my_csv.csv','a') as f:
-        df.to_csv('my_csv.csv', float_format='{:f}'.format, header=False, mode='a')
+        f = script_path + '\my_csv.csv'
+        df.to_csv(f, float_format='{:f}'.format, header=False, mode='a')
     else:
         print('No new tweets')
 
@@ -57,7 +58,8 @@ def scrapeTwitter(df, api):
 
 
 def main():
-    keysFile = open("keys.txt", "r")
+    script_path = os.path.dirname(os.path.abspath(__file__))
+    keysFile = open((script_path + "\keys.txt"), "r")
     keys = keysFile.read().splitlines()
     keysFile.close()
     CONSUMER_KEY = keys[0]
@@ -71,9 +73,8 @@ def main():
     df = pd.DataFrame(columns=[
         'user', 'retweeted', 'favorited', 'quoted', 'normal', 'id', 'timestamp'
     ])
-    f = 'my_csv.csv'
+    f = script_path + '\my_csv.csv'
     if not os.path.isfile(f):
-        print(False)
         df.to_csv(f, float_format='{:f}'.format, header='column_names')
     else:
         df = pd.read_csv(f)
